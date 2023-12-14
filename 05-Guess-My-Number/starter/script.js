f7164e70fdf4303456a78507f10096163e88fbd8
SalmanKoradiya <mikaelkoradiya@gmail.com>
2023-12-14 17:30:57 +0530

    Day 7 Guess Game Complete

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,25 +1,52 @@
 'use strict';
 
-// console.log(document.querySelector('.message').textContent);
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
-// document.querySelector('.message').textContent = 'Correct Number';
+let score = 20; // state variable
 
-// document.querySelector('.number').textContent = 13;
-// document.querySelector('.score').textContent = 10;
-
-// document.querySelector('.guess').value = 23;
-// console.log(document.querySelector('.guess').value);
-
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = secretNumber;
+let highScore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess);
+
+  if (!guess) {
+    // guess not equal
+    document.querySelector('.message').textContent = ' No Number!!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number!';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = secretNumber;
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highestscore').textContent = highScore;
+    }
+  } else if (guess !== secretNumber) {
+    if (score !== secretNumber) {
+      document.querySelector('.message').textContent =
+        guess > secretNumber ? 'Too High' : 'Too Low';
+      score = score - 1; // Old Pattern
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You Lost The Game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
 });
 
-if (!guess) {
-  document.querySelector('.message').textContent = ' No Number!!';
-} else if (guess === secretNumber) {
-  document.querySelector('.message').textContent = 'Correct Number';
-}
+// Reset Game
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  document.querySelector('.message').textContent = 'Start Guessing...';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
